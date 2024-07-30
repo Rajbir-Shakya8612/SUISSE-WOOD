8ea0d8bb69011e59d5e3786266d778c517f6f08c
Rajbir Shakya <arbrainentertainment@gmail.com>
2024-07-30 15:03:43 +0530

    feat : add admin panel layouts and content page

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -72,6 +72,7 @@
     <script src="lib/owlcarousel/owl.carousel.min.js"></script>
     <script src="lib/isotope/isotope.pkgd.min.js"></script>
     <script src="lib/lightbox/js/lightbox.min.js"></script>
+    <script src="js/main.js"></script>
 
 
 </body>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/views/admin-panel/vendor/fontawesome-free/css/all.min.css",
+                "resources/views/admin-panel/vendor/jquery/jquery.min.js",
+                "resources/views/admin-panel/vendor/jquery-easing/jquery.easing.min.js",
+                "resources/views/admin-panel/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "resources/views/admin-panel/css/admin.min.css",
+                "resources/views/admin-panel/js/admin.min.js",
+                "resources/views/admin-panel/js/demo/datatables-demo.js",
+            ],
             refresh: true,
         }),
     ],
